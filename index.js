fbb1a5f42f069ba60cf240ad762c2b990d62b002
Fernando <fbanarescarrasco@gmail.com>
2022-09-09 12:31:04 -0300

    desafio 1

--- index.js
+++ index.js
@@ -4,43 +4,49 @@ class persona {
         this.apellido = apellido;
         this.libros = libros
         this.mascotas = mascotas;
-    }    
+    } 
+  
+    getFullName() {
+        return `El nombre de usuario es ${this.nombre} ${this.apellido}`;
+    }
+    
+    addMascota(){
+        this.mascotas.push();
+    }
+    
+    countMascotas(){
+       return `Tengo ${this.mascotas.lenght} mascotas`;
+    }
+    
+    addBook(titulo, autor){
+        const libro = {titulo: titulo,autor: autor}
+        this.libros.push(libro)
+    }
+
+    countBooks(){
+        return  `Tengo ${this.libros.length} libros`;
+    }
+    
+    getBookNames(){
+        let arrayResult = []
+        this.libros.forEach(element => {
+            arrayResult.push(element.nombre)
+        });
+        return arrayResult
+    }
+    
 }
 
-function getFullName(){
-    return(`${this.nombre} ${this.apellido}`)
-}
-
-function addMascota(){
-    this.mascotas.push();
-}
-
-function countMascotas(){
-   return this.mascotas.lenght;
-}
-
-function addBook(){
-    this.libros.push()
-}
-
-function getBookNames(){
-    let arrayResult = []
-    this.libros.forEach(element => {
-        arrayResult.push(element.nombre)
-    });
-    return arrayResult
-}
 
 
+const usuario1 = new persona('Fernando', 'Bañares',{titulo:'Farenheit 451', autor:'Ray Bradbury'},['Luna','Diana'])
 
-
-
-let libros = {nombre: 'Hola', autor: 'pepe'}
-let mascota1= 'canario'
-let usuario = new persona('Fernando', 'Bañares',{nombre:'Farenheit 451', autor:'Ray Bradbury'},['Luna','Diana'])
-
-addBook(libros)
-
-
-console.log(usuario)
+console.log(usuario1.getFullName());
+usuario1.addMascota('Totu');
+console.log(usuario1.mascotas);
+console.log(usuario1.countMascotas());
+usuario1.addBook('Spiderman','Lucas');
+console.log(usuario1.libros);
+console.log(usuario1.getBookName());
+console.log(usuario1.countBooks());
 
